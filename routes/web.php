8753949c02583ee3b31117383a51fea60383944e
madummuhammad <muhammad.madum2018@gmail.com>
2021-11-21 22:32:11 +0700

    init

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/',[AuthController::class,'index'])->middleware('guest')->name('login');
+Route::post('/',[AuthController::class,'authenticate']);
+
+Route::middleware('auth')->group(function(){
+    Route::get('/dashboard',[DashboardController::class,'index']);
+
+    Route::post('/logout',[AuthController::class,'logout']);
+});
